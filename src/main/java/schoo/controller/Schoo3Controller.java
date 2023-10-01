0980a693bb2f61d1a7d5fc3bf4b42cf9816f674f
Kono <konotori08@gmail.com>
2023-10-01 18:15:21 +0900

    Second Commit

--- src/main/java/schoo/controller/Schoo3Controller.java
+++ src/main/java/schoo/controller/Schoo3Controller.java
@@ -8,15 +8,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Schoo3Controller {
-	
+	/**
+	 * トップページのURLリクエスト
+	 * @return
+	 */
 	@GetMapping("/")
 	public String index() {
 		return "input";
 	}
-	
+	/**
+	 * 入力処理リクエスト
+	 * @param 入力メッセージ
+	 * @return 結果画面のパス(output)
+	 */
 	@PostMapping("input")
 	public String input(@RequestParam String message,
 			             Model model) {
+		// input画面で入力されたメッセージをModelにセット
 		model.addAttribute("message", message);
 		return "output";
 	}
